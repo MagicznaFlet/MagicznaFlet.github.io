837f96d4f312865f69603af0b062cf77c8ecab73
FilipUrzon <filip.urzon2003@gmail.com>
2024-06-11 15:01:55 +0200

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import BackgroundAnimation from './components/BackgroundAnimation'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>Hello its me</p>
+        <BackgroundAnimation></BackgroundAnimation>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/BackgroundAnimation.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function BackgroundAnimation():JSX.Element{
+    return (
+        <div>
+            <p>Animation</p>
+        </div>
+    )
+}
+
+export default BackgroundAnimation
