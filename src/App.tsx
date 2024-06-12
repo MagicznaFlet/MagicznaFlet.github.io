7592980eb1ab757f0fb93cd6c9832d2bd669f56e
FilipUrzon <filip.urzon2003@gmail.com>
2024-06-12 10:27:21 +0200

    Added basic model

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-
+import { useState} from 'react'
 import './App.css'
 import BackgroundAnimation from './components/BackgroundAnimation'
 
 function App() {
-
   return (
     <>
       <div>
--- src/components/BackgroundAnimation.tsx
+++ src/components/BackgroundAnimation.tsx
@@ -1,9 +1,34 @@
-import React from "react"
+import {Suspense} from "react"
+import { useLoader,Canvas,SceneProps } from "@react-three/fiber"
+import { GLTFLoader } from "three/examples/jsm/Addons.js"
+import { Environment, OrbitControls, useGLTF } from "@react-three/drei"
 
-function BackgroundAnimation():JSX.Element{
+function FallbackComponent(props:any){
+    return(
+        <>
+        <div>
+            <p>FallbackComponent</p>
+            <img src="/image.png" alt="Cat picture" />
+        </div>
+        </>
+    )
+}
+
+function Model(props:any){
+    const backgroundModel = useGLTF('/dog.glb')
+    return <primitive object={backgroundModel.scene} {...props}/>;
+}
+
+function BackgroundAnimation(props:any){
     return (
         <div>
-            <p>Animation</p>
+                <Canvas camera={{position:[5,5,-5],fov:25}}>
+                    <Suspense fallback={<FallbackComponent/>}/>
+                    <Environment preset="city"/>
+                    <ambientLight intensity={0.5}/>
+                    <Model position={[-0.1,-0.2,0]} rotation={[0,Math.PI / 2,0]} scale={0.2}/>
+                    <OrbitControls minPolarAngle={Math.PI / 2.5} maxPolarAngle={Math.PI / 2.5} />
+                </Canvas>
         </div>
     )
 }
