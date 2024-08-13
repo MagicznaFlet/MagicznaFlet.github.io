cdb197e2971b91e88dc6cbe99a7f77982d040787
Filip Urzoń <filip.urzon2003@gmail.com>
2024-08-14 00:01:40 +0200

    Improved image display

--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -4,46 +4,41 @@ import { motion } from 'framer-motion'
 export default function Projects() {
     return (
         <motion.div
-            initial={{ y: 50 }}
-            animate={{ y: 0 }}
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
         >
             <Helmet>
                 <title>Projects - Filip Urzoń</title>
-                <meta name="description" content="Projects"></meta>
-                <meta name='keywords' content="Filip Urzoń, skills, " />
+                <meta name="description" content="Projects" />
+                <meta name='keywords' content="Filip Urzoń, projects " />
             </Helmet>
-            <div className=" ">
-                <h1 className="text-3xl ">Projects</h1>
-                <p>Passionate about technology, programming and and databases. Ocasionally doing blender. I like books (especially Stephen's King and Dune series)
-                    <p></p>
-                </p>
-                <div className="w-full flex flex-col md:flex-row  flex-wrap items-center md:justify-center w-full md:gap-10">
-                    <div className="w-[80%] md:w-[220px] min-h-[175px] text-center">
-                        <p className="bg-background">Text</p>
-                        <img src="catimg.png" alt="" className="w-full h-[175px] rounded-xl" />
+
+            <h1 className="text-3xl ">Projects</h1>
+            <h2>
+                Stuff that i make
+            </h2>
+            <div className="w-full flex flex-col md:flex-row  flex-wrap items-center md:justify-center md:gap-10">
+                <div className="text-center mt-5 mb-5 w-full md:max-w-[220px]">
+                    <div className="w-full md:w-[220px] md:max-h-[175px] h-auto">
+                        <img src="catimg.png" alt="Yelpcamp" className="w-full h-auto object-cover rounded-xl" />
                     </div>
-                    <div className="w-[80%] md:w-[220px] min-h-[175px] ">
-                        <p className="bg-background">Text</p>
-                        <img src="catimg.png" alt="" className="w-full h-[175px] rounded-xl" />
-                    </div>
-                    <div className="w-[80%] md:w-[220px] min-h-[175px] ">
-                        <p className="bg-background">Text</p>
-                        <img src="catimg.png" alt="" className="w-full h-[175px] rounded-xl" />
-                    </div>
-                    <div className="w-[80%] md:w-[220px] min-h-[175px]">
-                        <p className="bg-background">Text</p>
-                        <img src="catimg.png" alt="" className="w-full h-[175px] rounded-xl" />
-                    </div>
-                    <div className="w-[80%] md:w-[220px] min-h-[175px] ">
-                        <p className="bg-background">Text</p>
-                        <img src="catimg.png" alt="" className="w-full h-[175px] rounded-xl" />
-                    </div>
-                    <div className="w-[80%] md:w-[220px] min-h-[175px] ">
-                        <p className="bg-background">Text</p>
-                        <img src="catimg.png" alt="" className="w-full h-[175px] rounded-xl" />
-                    </div>
+                    <p className="bg-background">YelpCamp</p>
                 </div>
+                <div className="text-center mt-5 mb-5 w-full md:max-w-[220px]">
+                    <div className="w-full md:w-[220px] md:max-h-[175px] h-auto">
+                        <img src="catimg.png" alt="Yelpcamp" className="w-full h-auto object-cover rounded-xl" />
+                    </div>
+                    <p className="bg-background">YelpCamp</p>
+                </div>
+                <div className="text-center mt-5 mb-5 w-full md:max-w-[220px]">
+                    <div className="w-full md:w-[220px] md:max-h-[175px] h-auto">
+                        <img src="catimg.png" alt="Yelpcamp" className="w-full h-auto object-cover rounded-xl" />
+                    </div>
+                    <p className="bg-background">YelpCamp</p>
+                </div>
+
             </div>
+
         </motion.div>
     )
 }
